3f0bc2e4d3ec4d784d16be95e4013b877a1f4506
laurijuhani <lauri.wilppu@gmail.com>
2025-06-14 12:21:29 +0300

    1.5

--- the_project/src/index.ts
+++ the_project/src/index.ts
@@ -3,6 +3,9 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (_req, res) => {
+  res.send('Hello, World!');
+});
 
 
 app.listen(PORT, () => {
