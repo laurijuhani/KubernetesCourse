cea93ed1e69662152b504af0b2a7de17c53cf87c
laurijuhani <lauri.wilppu@gmail.com>
2025-06-18 12:40:49 +0300

    1.12

--- the_project/src/index.ts
+++ the_project/src/index.ts
@@ -1,10 +1,55 @@
 import express from 'express';
+import fs from 'fs';
+import path from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const CACHE_DIR = '/app/cache';
+const IMAGE_FILE = path.join(CACHE_DIR, 'image.jpg');
+const META_FILE = path.join(CACHE_DIR, 'meta.json');
+const CACHE_DURATION = 10 * 60 * 1000;
+
+interface MetaData {
+  lastFetched: number;
+}
+
+const fetchAndCacheImage = async () => {
+  const res = await fetch('https://picsum.photos/1200');
+  const arrayBuffer = await res.arrayBuffer();
+  const buffer = Buffer.from(arrayBuffer);
+  fs.writeFileSync(IMAGE_FILE, buffer); 
+  const metaData: MetaData = { lastFetched: Date.now() };
+  fs.writeFileSync(META_FILE, JSON.stringify(metaData));
+};
+
+const shouldFetchNewImage = () => {
+  if (!fs.existsSync(IMAGE_FILE) || !fs.existsSync(META_FILE)) {
+    return true;
+  }
+  const metaData: MetaData = JSON.parse(fs.readFileSync(META_FILE, 'utf-8'));
+  const currentTime = Date.now();
+  return (currentTime - metaData.lastFetched) > CACHE_DURATION;
+}
+
+
+app.get('/image', async (_req, res) => {
+  try {
+    if (shouldFetchNewImage()) {
+      await fetchAndCacheImage();
+    } 
+    res.sendFile(IMAGE_FILE);
+  } catch (err) {
+    res.status(500).send('Failed to fetch or serve image');
+  }
+});
+
 app.get('/', (_req, res) => {
-  res.send('Hello, World!');
+  res.send(`
+    <h1>The project App</h1>
+    <img src="/image" style="max-width:100%;height:auto;" />
+    <p>DevOps with Kubernetes 2025</p>
+  `);
 });
 
 
